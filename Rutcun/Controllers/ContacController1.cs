dcf2b5aaa81378025b96fa348483d0f0cba0eb89
RutcunOficial <rutcunoficial@gmail.com>
2022-11-29 09:22:17 -0500

    Se agrego pagina de contactos

---
+++ Rutcun/Controllers/ContacController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rutcun.Controllers
+{
+    public class ContacController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
